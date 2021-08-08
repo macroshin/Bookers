8a4e89cb335ecab842bfc76bb74f2c8523a5f4b8
macroshin <sckm0315@gmail.com>
2021-08-08 15:49:46 +0000

    [ADD] vali,flash 3-8

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
+    @books = Book.all
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book), notice: 'Book was successfully created.'
+    else
+      render :index
+    end
   end
 
   def edit
@@ -19,15 +23,18 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: 'Book was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
     book.destroy
-    redirect_to books_path
+    redirect_to books_path, notice: 'Book was successfully destroyed.'
   end
 
   private
